489dc2219233df1bda43dd4d90c9c5abf3a5c526
Ali <aasaidi54@gmail.com>
2022-02-07 03:23:47 +0100

    Changed all code and refactored the script.

--- main.py
+++ main.py
@@ -2,22 +2,56 @@
 
 # Modules & libraries.
 import requests as rq
-import logging
-from icrawler.examples import GoogleImageCrawler
+from PIL import Image, ImageDraw
+# from icrawler.examples import GoogleImageCrawler
+
+
+def main() : 
+    # crawlImages()
+    createNFT()
 
 def crawlImages() : 
     # goal example : ronaldo goal vs moreirense 2:0 solo run 2002/03
-    google_crawler = GoogleImageCrawler('./images/')
-    google_crawler.crawl(keyword='ronaldo goal vs moreirense 2:0 solo run 2002/03', offset=0, max_num=1, date_min=None, date_max=None,
-        feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4, min_size=(400,400), max_size=None)
-
-def createNFT() : 
+    # google_crawler = GoogleImageCrawler('./images/')
+    # google_crawler.crawl(keyword='ronaldo goal vs moreirense 2:0 solo run 2002/03', offset=0, max_num=1, date_min=None, date_max=None,
+    #     feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4, min_size=(400,400), max_size=None)
     pass
 
-def main() : 
-    crawlImages()
-    createNFT()
+def createNFT() : 
+    drawGoalImage()
+    drawEmptyRectangle()
 
+
+# ------------------------------------------------------
+# ------------------------------------------------------
+# ------------------------------------------------------
+
+def drawGoalImage() : 
+    pass
+
+def drawEmptyRectangle() : 
+    w, h = 160, 160
+    shape = [(0, 0), (w, h)]
+    img = Image.new("RGB", (w, h))
+    img1 = ImageDraw.Draw(img)
+    img1.rectangle(shape, fill ="white")
+    img.save("./example.jpg")
+    print("finished!")
+
+def get_concat_h(im1, im2):
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+def get_concat_v(im1, im2):
+    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+# get_concat_h(im1, im1).save('data/dst/pillow_concat_h.jpg')
+# get_concat_v(im1, im1).save('data/dst/pillow_concat_v.jpg')
 
 
 # Main of the program.
@@ -25,6 +59,4 @@ if __name__ == "__main__" :
     try : 
         main()
     except(KeyboardInterrupt) :
-        print("\n") 
-        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-        logging.error("Script finished by keyboard interrupt")
+        print("\nERROR: KeyboardInterrupt")
