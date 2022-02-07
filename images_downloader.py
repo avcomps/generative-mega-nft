bc692a76f430a1f1cac5101a1df74e1e4c2d5e55
Ali <aasaidi54@gmail.com>
2022-02-07 02:42:30 +0100

    Another one. Cleaned garbage

--- images_downloader.py
+++ images_downloader.py
@@ -2,29 +2,14 @@
 
 import requests as rq
 from bs4 import BeautifulSoup
+from icrawler.examples import GoogleImageCrawler
 
-PLAYER_URL = "https://www.transfermarkt.com/cristiano-ronaldo/alletore/spieler/8198/plus/0?saison=&verein=&liga=&wettbewerb=&pos=&minute=&pos=&torart=&stand="
 headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' }
 # goal example : ronaldo goal vs moreirense 2:0 solo run 2002/03
 
+google_crawler = GoogleImageCrawler('./images/')
+google_crawler.crawl(keyword='ronaldo goal vs moreirense 2:0 solo run 2002/03', offset=0, max_num=1, date_min=None, date_max=None,
+    feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4, min_size=(400,400), max_size=None)
 
-html_res = rq.get(PLAYER_URL, headers=headers).text
-
-print("----------------------------------------")
-
-soup = BeautifulSoup(html_res, features="lxml")
-table = soup.find("table", attrs={"class":"details"})
-
-print(soup)
-print("----------------------------------------")
-print(table)
-print("----------------------------------------")
-
-# The first tr contains the field names.
-headings = [th.get_text() for th in table.find("tr").find_all("th")]
-
-for row in table.find_all("tr")[1:]:
-    print(row)
-    print("111111111111111111111110000000000000000000")
 
 
--- main.py
+++ main.py
@@ -2,28 +2,27 @@
 
 # Modules & libraries.
 import requests as rq
-import cv2 as cv
 import logging
+from icrawler.examples import GoogleImageCrawler
 
-# Procedures & functions.
+
 
 def main() : 
-    # Search for all images according to number of Ronaldo's goals: 803 scored.
-    searchImage()
-    downloadImage()
-    imageTreatment()
+    # goal example : ronaldo goal vs moreirense 2:0 solo run 2002/03
+    google_crawler = GoogleImageCrawler('./images/')
+    google_crawler.crawl(keyword='ronaldo goal vs moreirense 2:0 solo run 2002/03', offset=0, max_num=1, date_min=None, date_max=None,
+        feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4, min_size=(400,400), max_size=None)
 
-    url = "https://www.transfermarkt.com/cristiano-ronaldo/alletore/spieler/8198/plus/0?saison=&verein=&liga=&wettbewerb=&pos=&minute=&pos=&torart=&stand="
-    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' }
-    response = rq.get(url, headers = headers).text
-    print(response)
 
-def getImage() : 
-    pass
+
+
+
+
+
+
 
 
 # Main of the program.
-
 if __name__ == "__main__" : 
     try : 
         main()
