2709fbf0d3028ce442f3f75185cb7579394fb7ba
Ali <aasaidi54@gmail.com>
2022-02-07 02:20:55 +0100

    dlasmfldm

--- images_downloader.py
+++ images_downloader.py
@@ -1,10 +1,13 @@
 # Copyright @ Ali Vorobiev (https://github.com/avcomps).
 
+from ctypes.wintypes import PLARGE_INTEGER
 import requests as rq
 from bs4 import BeautifulSoup
 
 PLAYER_URL = "https://www.transfermarkt.com/cristiano-ronaldo/alletore/spieler/8198/plus/0?saison=&verein=&liga=&wettbewerb=&pos=&minute=&pos=&torart=&stand="
 headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' }
+# goal example : ronaldo goal vs moreirense 2:0 solo run 2002/03
+
 
 html_res = rq.get(PLAYER_URL, headers=headers).text
 
@@ -21,14 +24,8 @@ print("----------------------------------------")
 # The first tr contains the field names.
 headings = [th.get_text() for th in table.find("tr").find_all("th")]
 
-datasets = []
 for row in table.find_all("tr")[1:]:
     print(row)
-    dataset = zip(headings, (print(td.get_text()) for td in row.find_all("td")))
-    datasets.append(dataset)
+    print("111111111111111111111110000000000000000000")
 
-print (datasets)
-
-# with open("goals.txt",'w',encoding = 'utf-8') as f:
-#     f.write("my first file\n")
 
