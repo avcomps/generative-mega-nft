dae9a7ff2050fbf57dc7839fece9802158f6e4c8
Ali <aasaidi54@gmail.com>
2022-02-07 03:02:59 +0100

    example

--- main.py
+++ main.py
@@ -3,20 +3,20 @@
 # Modules & libraries.
 import requests as rq
 import logging
-from icrawler import GoogleImageCrawler
+from icrawler.examples import GoogleImageCrawler
 
-def main() : 
+def crawlImages() : 
     # goal example : ronaldo goal vs moreirense 2:0 solo run 2002/03
     google_crawler = GoogleImageCrawler('./images/')
     google_crawler.crawl(keyword='ronaldo goal vs moreirense 2:0 solo run 2002/03', offset=0, max_num=1, date_min=None, date_max=None,
         feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4, min_size=(400,400), max_size=None)
 
+def createNFT() : 
+    pass
 
-
-
-
-
-
+def main() : 
+    crawlImages()
+    createNFT()
 
 
 
