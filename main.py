6e157a1ca768dd4a5dfade5ae917fe0e40788ae3
Ali <aasaidi54@gmail.com>
2022-02-07 22:52:38 +0100

    Corrected indention

--- main.py
+++ main.py
@@ -13,20 +13,19 @@ def draw_white_square():
     img1.rectangle(shape, fill="white")
     return img
 
-
 def get_concat_h(im1, im2):
     dst = Image.new('RGB', (im1.width + im2.width, im1.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (im1.width, 0))
-    return dst
 
+    return dst
 
 def get_concat_v(im1, im2):
     dst = Image.new('RGB', (im1.width, im1.height + im2.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (0, im1.height))
+    
     return dst
-
 
 def crawl_images():
     print
@@ -40,10 +39,8 @@ def create_NFT():
         for j in range(28):
             if j == 0:
                 image_segment_h = draw_white_square()
-                pass
             else:
-                image_segment_h = get_concat_h(
-                    draw_white_square(), image_segment_h)
+                image_segment_h = get_concat_h(draw_white_square(), image_segment_h)
 
         image_final_NFT = get_concat_v(image_segment_h, image_final_NFT)
 
